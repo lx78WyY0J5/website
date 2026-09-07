a188bf1c5476378ffb4c4dde867915a75fde2f38
lx78WyY0J5 <9j2k37st@gmail.com>
2026-09-07 23:23:12 +0200

    base html

--- src/index/index.php
+++ src/index/index.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Site Web</title>
-    <!-- Lien vers le fichier CSS principal -->
-<link rel="stylesheet" href="./index.css">
-<!-- Script JavaScript principal -->
-<script src="./index.js" defer></script>
-</head>
-<body>
-    <h1>Bienvenue sur notre site web</h1>
-    <?php include "./main.php"; ?>
-</body>
+
+    <head>
+        <meta charset="UTF-8">
+        <title>Site Web</title>
+
+        <link rel="stylesheet" href="./index.css">
+        <script src="./index.js"></script>
+    </head>
+
+    <body>
+        <h1>Bienvenue sur notre site web</h1>
+        <?php include "./main.php"; ?>
+    </body>
+
+    <footer>
+
+    </footer>
 </html>
