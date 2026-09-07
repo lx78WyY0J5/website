7fd42370fa237d82473d1b4a4e6dc0b49e21effa
lx78WyY0J5 <9j2k37st@gmail.com>
2026-09-07 23:02:24 +0200

    add debug info

--- src/index/index.php
+++ src/index/index.php
@@ -3,4 +3,18 @@
 // Hello World from PHP.
 
 echo '<p>Hello World from PHP!</p>';
+
+echo '<hr>';
+
+echo '<p>PHP version : ' . phpversion() . '</p>';
+
+echo '<hr>';
+
+echo '<p>OS : ' . PHP_OS . '</p>';
+echo '<p>' . trim(shell_exec("whoami")) . '@' . shell_exec("hostnamectl hostname") . ' : ' . shell_exec("date") . '</p>';
+echo '<p>Uname -a : ' . shell_exec('uname -a') . '</p>';
+
+echo '<hr>';
+
+echo '<p>Document root : ' . $_SERVER['DOCUMENT_ROOT'] . '</p>';
 ?>
