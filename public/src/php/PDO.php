3065d27c06bbd074aa5c227880af00430fae8604
lx78WyY0J5 <9j2k37st@gmail.com>
2026-09-08 20:42:12 +0200

    ident & use .env getEnv

--- public/src/php/PDO.php
+++ public/src/php/PDO.php
@@ -1,15 +1,15 @@
 <?php
-$servername = "127.0.0.1";
-$username = "webuser";
-$password = "strongpassword";
-$dbname = "mydb";
+  $servername = "127.0.0.1";
+  $username = getenv('DB_USER');
+  $password = getenv('DB_PASS');
+  $dbname = getenv('DB_NAME');
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully to db : " . $dbname . '@' . $servername;
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
-}
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully to db : " . $dbname . '@' . $servername;
+  } catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+  }
 ?>
--- public/src/php/create-db.php
+++ public/src/php/create-db.php
@@ -1,25 +1,26 @@
 <?php
-$servername = "127.0.0.1";
-$username = "webuser";
-$password = "strongpassword";
+  $servername = "127.0.0.1";
+  $username = getenv('DB_USER');
+  $password = getenv('DB_PASS');
+  $dbname = getenv('DB_NAME');
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
-      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      } catch(PDOException $e) {
-        die("Could not connect. " . $e->getMessage());
-        }
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch(PDOException $e) {
+    die("Could not connect. " . $e->getMessage());
+  }
 
-        try {
-          $sql = "CREATE DATABASE myDB";
-            $conn->exec($sql);
-              echo "Database created successfully";
-              } catch(PDOException $e) {
-                // Handle errors during db creation
-                  echo "Error creating database: " . $sql . "<br>" . $e->getMessage();
-                  }
+  try {
+    $sql = "CREATE DATABASE " . $dbname;
+    $conn->exec($sql);
+    echo "Database created successfully";
+  } catch(PDOException $e) {
+    // Handle errors during db creation
+    echo "Error creating database: " . $sql . "<br>" . $e->getMessage();
+  }
 
-                  // Close connection
-                  $conn = null;
+  // Close connection
+  $conn = null;
 ?>
