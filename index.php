0e91f0b84ea16702ba89430fcdcdfd8a2f9fc389
lx78WyY0J5 <9j2k37st@gmail.com>
2026-09-08 03:05:08 +0200

    path update & customisable OG:meta

--- index.php
+++ index.php
@@ -1,35 +1,19 @@
 <?php
-$uri = trim($_SERVER['REQUEST_URI'], '/');
+    $uri = trim($_SERVER['REQUEST_URI'], '/');
 
-if ($uri === '' || $uri === 'accueil') {
-    $pageFile = __DIR__ . '/src/pages/accueil.php';
-}
-else {
-    $filePath = __DIR__ . '/src/pages/' . str_replace('/', DIRECTORY_SEPARATOR, $uri) . '.php';
-    if (file_exists($filePath)) {
-        $pageFile = $filePath;
+    if ($uri === '' || $uri === 'accueil') {
+        $pageFile = $_SERVER['DOCUMENT_ROOT'] . '/src/index/index.php';
     }
     else {
-        http_response_code(404);
-        echo '404 Not Found';
+        $filePath = $_SERVER['DOCUMENT_ROOT'] . '/src/pages/' . str_replace('/', DIRECTORY_SEPARATOR, $uri) . '.php';
+        if (file_exists($filePath)) {
+            $pageFile = $filePath;
+        }
+        else {
+            http_response_code(404);
+            echo '404 Not Found';
+        }
     }
-}
-?>
 
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <title>Site Web</title>
-        <link rel="stylesheet" href="src/index/index.css">
-        <script src="src/index/index.js"></script>
-    </head>
-    <body>
-        <?php
-            include __DIR__ . '/src/includes/header/header.html';
-            include $pageFile;
-            include __DIR__ . '/src/php/info.php';
-            include __DIR__ . '/src/includes/footer/footer.html';
-        ?>
-    </body>
-</html>
+    include $pageFile;
+?>
---
+++ src/index/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/src/index/head.html'; ?>
+    <body>
+        <?php
+            include $_SERVER['DOCUMENT_ROOT'] . '/src/includes/header/header.html';
+            include $_SERVER['DOCUMENT_ROOT'] . '/src/index/index.html';
+            include $_SERVER['DOCUMENT_ROOT'] . '/src/includes/footer/footer.html';
+            include $_SERVER['DOCUMENT_ROOT'] . '/src/php/info.php';
+        ?>
+    </body>
+</html>
