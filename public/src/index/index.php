dfc6ee92e4835698e863bc7e1c1a51435b07d4ee
lx78WyY0J5 <9j2k37st@gmail.com>
2026-09-11 17:09:26 +0200

    add login or register CTA

--- public/src/index/index.php
+++ public/src/index/index.php
@@ -5,6 +5,9 @@
 
     if($_SESSION["is_admin"] !== true){
         include $_SERVER['DOCUMENT_ROOT'] . '/src/index/index.html';
+        if(!isset($_SESSION['loggedin'])){
+            echo '<p><a href="/login">Connectez-vous</a> ou <a href="/register">créez un compte</a></p>';
+        }
     }
     else{
         include $_SERVER['DOCUMENT_ROOT'] . '/src/pages/info/info.php';
