767974cc8e0b95f699381b03bd66d0b56fe619aa
lx78WyY0J5 <9j2k37st@gmail.com>
2026-09-11 15:54:10 +0200

    refactor index styling and global CSS

--- public/index.php
+++ public/index.php
@@ -30,6 +30,7 @@
 <html lang="fr">
     <?php include $_SERVER['DOCUMENT_ROOT'] . '/src/pages/' . str_replace('/', DIRECTORY_SEPARATOR, $uri) . '/head.html'; ?>
     <link rel="stylesheet" href="/src/css/font.css">
+    <link rel="stylesheet" href="/src/css/style.css">
     <link rel="stylesheet" href="/src/css/scrollbar.css">
     <body>
         <link rel="stylesheet" href="/src/css/theme.css">
