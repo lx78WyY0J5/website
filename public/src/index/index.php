f194d4ea4b09b8486e9bef2872a50afe2567be91
lx78WyY0J5 <9j2k37st@gmail.com>
2026-09-11 16:01:45 +0200

    index change if logged in or admin

--- public/src/index/index.php
+++ public/src/index/index.php
@@ -1,6 +1,15 @@
 <?php
-    include $_SERVER['DOCUMENT_ROOT'] . '/src/index/index.html';
     include $_SERVER['DOCUMENT_ROOT'] . '/src/php/loadEnv.php';
     include $_SERVER['DOCUMENT_ROOT'] . '/src/php/PDO.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/src/php/info.php';
+
+    if(isset($_SESSION["loggedin"]) && isset($_SESSION['username'])){
+        echo '<h2>Bienvenue ' . $_SESSION['username'] .'</h2>';
+    }
+
+    if($_SESSION["is_admin"] !== true){
+        include $_SERVER['DOCUMENT_ROOT'] . '/src/index/index.html';
+    }
+    else{
+        include $_SERVER['DOCUMENT_ROOT'] . '/src/php/info.php';
+    }
 ?>
