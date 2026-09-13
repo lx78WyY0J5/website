4585302feaa4d9478aebdad3177c69c4f9c7a3bc
lx78WyY0J5 <9j2k37st@gmail.com>
2026-09-14 01:16:59 +0200

    new test added

---
+++ tests/test_weak_password_low_entropy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Test: Weak password (low entropy) on /register
+BASE_URL="${BASE_URL:-http://127.0.0.1:8000}"
+
+echo "=== test_weak_password_low_entropy.sh ==="
+
+resp=$(curl -s -X POST "$BASE_URL/register" \
+  -d "username=entropyuser&password=StrongPass123!&confirm_password=StrongPass123!&code=123456" \
+  -H "Content-Type: application/x-www-form-urlencoded")
+
+echo "Response: $resp"
+
+echo "$resp" | grep -q "Le mot de passe n'est pas assez fort\|entropy\|Entropie"
+exit $?
