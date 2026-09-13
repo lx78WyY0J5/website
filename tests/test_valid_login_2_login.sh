3178dd871875b4be5a7dbfa4ac3322e6d9f5e3c1
lx78WyY0J5 <9j2k37st@gmail.com>
2026-09-14 01:11:31 +0200

    removed show response as it now work

--- tests/test_valid_login_2_login.sh
+++ tests/test_valid_login_2_login.sh
@@ -8,7 +8,5 @@ resp=$(curl -s -X POST "$BASE_URL/login" \
   -d "username=testuser123&password=StrongPass123!" \
   -H "Content-Type: application/x-www-form-urlencoded")
 
-echo "Response: $resp"
-
 echo "$resp" | grep -q "location: /\|window.location.href = '/'"
 exit $?
