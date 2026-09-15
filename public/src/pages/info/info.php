e377ad0b5565bffd27f9b219fbb5fbb1d79e076a
lx78WyY0J5 <9j2k37st@gmail.com>
2026-09-15 22:33:23 +0200

    fix for some OS (that wont handle getSysAVG

--- public/src/pages/info/info.php
+++ public/src/pages/info/info.php
@@ -4,37 +4,41 @@
         echo '<div id="info">';
             include $_SERVER['DOCUMENT_ROOT'] . '/src/pages/info/info.html';
 
-            echo '<pre>' . shell_exec("echo '{\"modules\":[]}' | fastfetch --logo arch --config -") . '</pre>';
+            echo '<pre>' . shell_exec("echo '{\"modules\":[]}' | fastfetch --logo logosource --config -") . '</pre>';
             echo '<p>PHP v°' . phpversion() . '</p>';
             echo '<p>OS : ' . PHP_OS . "   " . trim(shell_exec("whoami")) . '@' . shell_exec("hostnamectl hostname") . '</p>';
             echo '<p>' . shell_exec('uname -r') . '</p>';
 
             echo '<p>Document root : ' . $_SERVER['DOCUMENT_ROOT'] . '</p>';
 
-            $cpuLoad = sys_getloadavg();
-            $cpuUsage = $cpuLoad[0]; // 1-minute load average
-            echo '<p>CPU Average (1 min) : ' . $cpuUsage . '</p>';
+            try{
+                $cpuLoad = sys_getloadavg();
+                $cpuUsage = $cpuLoad[0]; // 1-minute load average
+                echo '<p>CPU Average (1 min) : ' . $cpuUsage . '</p>';
 
-            // RAM Usage
-            $free = shell_exec('free');
-            $free = (string)trim($free);
-            $free_arr = explode("\n", $free);
-            $mem = explode(" ", $free_arr[1]);
-            $mem = array_filter($mem);
-            $mem = array_merge($mem);
+                // RAM Usage
+                $free = shell_exec('free');
+                $free = (string)trim($free);
+                $free_arr = explode("\n", $free);
+                $mem = explode(" ", $free_arr[1]);
+                $mem = array_filter($mem);
+                $mem = array_merge($mem);
 
-            $memTotal = $mem[1];
-            $memUsed  = $mem[2];
-            $memUsagePercent = round(($memUsed / $memTotal) * 100, 2);
-            echo '<p>RAM : ' . round(($memUsed/1024/1024), 1) . 'Gb /' . round(($memTotal/1024/1024), 1) . 'Gb (' . $memUsagePercent . '%)</p>';
+                $memTotal = $mem[1];
+                $memUsed  = $mem[2];
+                $memUsagePercent = round(($memUsed / $memTotal) * 100, 2);
+                echo '<p>RAM : ' . round(($memUsed/1024/1024), 1) . 'Gb /' . round(($memTotal/1024/1024), 1) . 'Gb (' . $memUsagePercent . '%)</p>';
+            }
+            catch(Exception $e) {
+                echo '<p>Wooops, Sys_GetLoadAvg() ne marche pas ici !<br>' . e->getMessage() . ' </p>';
+            }
         echo '</div>';
         echo '<div id="db-create">';
             require_once $_SERVER['DOCUMENT_ROOT'] . '/src/php/create-db.php';
         echo '</div>';
         /*echo '<div id="php-info">';
-            phpinfo();
+            phpinfo(); //avoid PHP info as it override with some CSS
         echo '</div>';*/
-        //avoid PHP info as it override with some CSS
     }else{
         header("location: /");
         echo "<script>window.location.href = '/';</script>";
